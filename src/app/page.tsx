ae7fbc69521d16b83166a79d8bf13439b00fa9f8
Aaditya Srivastava <aaditya.srivastava.connect@gmail.com>
2025-03-18 18:14:57 +0530

    integrated kendo-react-ui components with button implimentation in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
+import { Button } from '@progress/kendo-react-buttons';
 
 export default async function Home() {
  return(
   <div>
-   <h1 className="font-bold">Hello World</h1>
+    <Button>
+      Hello World
+    </Button>
   </div>
  )
 }
